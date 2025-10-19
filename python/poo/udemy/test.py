0b5a64f8c2a6ef761e681778902a89d2d7397b33
Bruno <bruno.gnra@gmail.com>
2025-10-19 08:24:16 +0200

    update

---
+++ python/poo/udemy/test.py
@@ -0,0 +1,22 @@
+# for number in range(-3,0,1):
+#     print(number)
+
+
+# list_one = [1,2]
+# list_two = list_one[:]
+
+# numbers = [1,0.5, .25, .125]
+
+
+# print(numbers[numbers[1]])
+
+
+# the_data = [True, 3.14, -2]
+# # print(the_data[2:3])
+# print(the_data.index(-2))
+
+answers = (False, True, True)
+selection = answers[:]
+
+print(selection)
+print(selection)
