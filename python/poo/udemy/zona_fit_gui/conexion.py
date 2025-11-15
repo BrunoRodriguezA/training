8b0100dfd12e49b0e7bcf575cb0446bb75a9f836
Bruno <bruno.gnra@gmail.com>
2025-11-15 23:02:58 +0100

    update

---
+++ python/poo/udemy/zona_fit_gui/conexion.py
@@ -0,0 +1,63 @@
+import sys 
+from logger_base import log 
+from mysql.connector import pooling, Error
+
+
+class Conexion:
+    _DB_CONFIG = {
+        "host" : 'localhost',
+        "username" : 'root',
+        "password" : 'admin',
+        "database" : 'zona_fit_db',
+        "port" : '3306',
+        }
+    
+    _HOST = 'localhost'
+    _USERNAME = 'root'
+    _PASSWORD = 'admin'
+    _DATABASE  = 'zona_fit_db'
+    _DB_PORT = '3306'
+    _POOL_SIZE = 5
+    _POOL_NAME = 'zona_fit_pool'
+    _pool = None
+    
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                # cls._pool = pooling.MySQLConnectionPool(pool_name=cls._POOL_NAME, pool_size=cls._POOL_SIZE, **cls._DB_CONFIG)
+                cls._pool = pooling.MySQLConnectionPool(pool_name=cls._POOL_NAME, pool_size=cls._POOL_SIZE,
+                                                        username=cls._USERNAME, password=cls._PASSWORD,
+                                                        database=cls._DATABASE, host=cls._HOST,
+                                                        port=cls._DB_PORT)
+                log.debug(f"Poolname: {cls._pool.pool_name}")
+                # log.debug(f"Poolsize: {cls._pool.pool_size}")
+                return cls._pool
+            except Error as e:
+                log.debug(f"Ocurrio un error al obtener el pool: {e}, {type(e)}")
+        else:
+            return cls._pool
+        
+    @classmethod
+    def obtenerConexion(cls):
+        try:
+            conn = cls.obtenerPool().get_connection()
+            return conn
+        except Exception as e:
+            log.debug(f"Error al obtener la conexión: {e}, {type(e)}")
+            sys.exit()
+
+            
+    @classmethod
+    def liberarConexion(cls, conn):
+        conn.close()
+        log.debug(f"Conexion devuelta al pool: {conn}")
+        
+if __name__ == "__main__":
+    con1 = Conexion.obtenerConexion()
+    # conn = Conexion.obtenerConexion()
+    # cursor = conn.cursor()
+    pool = Conexion.obtenerPool()
+    print(con1)
+    print(pool)
+    Conexion.liberarConexion(con1)
