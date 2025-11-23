2068f9448009b64fea1133c183405ccf0a27f823
Bruno <bruno.gnra@gmail.com>
2025-11-23 21:14:51 +0100

    update

--- python/poo/udemy/zona_fit_gui/zona_fit_app_gui.py
+++ python/poo/udemy/zona_fit_gui/zona_fit_app_gui.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 # from clienteDAO import ClienteDAO
 from clienteDAO import ClienteDAO
+from tkinter.messagebox import showerror, showinfo
+from cliente import Cliente
 
 class App(tk.Tk):
     
@@ -9,12 +11,15 @@ class App(tk.Tk):
     
     def __init__(self):
         super().__init__()
+        self.id_cliente = None
         self.configurar_ventana()
         self.configurar_grid()
         self.mostrar_titulo()
         self.mostrar_formulario()
-        self.mostrar_tabla()    
-        
+        self.cargar_tabla()    
+        self.mostrar_botones()
+            
+           
     def configurar_ventana(self):
         self.geometry('700x500')
         self.title('Zona Fit App')
@@ -32,15 +37,38 @@ class App(tk.Tk):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
     
+    
     def mostrar_titulo(self):
         etiqueta = ttk.Label(self, text='Zona Fit (GYM)', font=('Arial', 15), background=App._COLOR_VENTANA,
                              foreground='white')
         etiqueta.grid(row=0, column=0, columnspan=2, pady=30)
         
-    def mostrar_formulario(self):
-        ...
         
-    def mostrar_tabla(self):
+    def mostrar_formulario(self):
+        self.frame_forma = ttk.Frame()
+        # Nombre
+        nombre_l = ttk.Label(self.frame_forma, text='Nombre: ')
+        nombre_l.grid(row=0, column=0, sticky=tk.W, pady=30, padx=5)
+        self.nombre_t = ttk.Entry(self.frame_forma)
+        self.nombre_t.grid(row=0, column=1)
+        
+        # Apellido
+        apellido = ttk.Label(self.frame_forma, text='Apellido: ')
+        apellido.grid(row=1, column=0, sticky=tk.W, pady=30, padx=5)
+        self.apellido_t = ttk.Entry(self.frame_forma)
+        self.apellido_t.grid(row=1, column=1)
+        
+        # Membresia
+        membresia = ttk.Label(self.frame_forma, text='Membresia: ')
+        membresia.grid(row=2, column=0, sticky=tk.W, pady=30, padx=5)
+        self.membresia_t = ttk.Entry(self.frame_forma)
+        self.membresia_t.grid(row=2, column=1)
+        
+        # publicar el fram de forma 
+        self.frame_forma.grid(row=1, column=0)
+        
+        
+    def cargar_tabla(self):
         self.frame_tabla = ttk.Frame(self)
         # definimos estilo de la tabla 
         self.estilos.configure('Treeview', background='black', foreground='white',
@@ -71,11 +99,120 @@ class App(tk.Tk):
         self.tabla.configure(yscroll=scrollbar.set)
         scrollbar.grid(row=0, column=1, sticky=tk.NS)
         
+        # asociar el evento select 
+        self.tabla.bind('<<TreeviewSelect>>', self.cargar_cliente)
+        
         # publicamos la tabla (dentro del frame tabla)
         self.tabla.grid(row=0,column=0)
         # mostramos el frame de tabla 
         self.frame_tabla.grid(row=1, column=1, padx=20)
+    
+    
+    def mostrar_botones(self):
+        self.frame_botones = ttk.Frame()
+        # boton agregar
+        agregar_boton = ttk.Button(self.frame_botones, text='Guardar', command=self.validar_cliente)
+        agregar_boton.grid(row=0, column=0, padx=30)
+
+        # boton de eliminar 
+        eliminar_boton = ttk.Button(self.frame_botones, text='Eliminar', command=self.eliminar_cliente)
+        eliminar_boton.grid(row=0, column=1, padx=30)        
+        # boton de limpiar
+        limpiar_boton = ttk.Button(self.frame_botones, text='Limpiar', command=self.limpiar_formulario)
+        limpiar_boton.grid(row=0, column=2, padx=30) 
         
+        # aplicar un estilo a los botones 
+        self.estilos.configure('TButton', background='#005f73')
+        self.estilos.map('TButton', background=[('active', '#0a9396')])
+        # publicar el frame de botones 
+        self.frame_botones.grid(row=2, column=0, columnspan=2, pady=20)           
+           
+           
+    def validar_cliente(self):
+        # validar cmapos 
+        if (self.nombre_t.get() and self.apellido_t.get() and self.membresia_t.get()):
+            if self.validar_membresia():
+                self.guardar_cliente()
+            else:
+                showerror(title='Atencion', message='El valor de membresia NO es numérico')
+                self.membresia_t.delete(0,tk.END)
+                self.membresia_t.focus_set()
+        else:
+            showerror(title='Atencion', message='Debe llenar el formulario')
+            self.nombre_t.focus_set()
+        
+        
+    def validar_membresia(self):
+        try:
+            int(self.membresia_t.get())
+            return True
+        except:
+            return False
+
+
+    def guardar_cliente(self):
+        # recuperar valores de caja de texto
+        nombre = self.nombre_t.get()
+        apellido = self.apellido_t.get()
+        membresia = self.membresia_t.get()
+        
+        # validamos el valor del atributo de id lciente 
+        if self.id_cliente is None:
+            cliente = Cliente(nombre=nombre, apellido=apellido, membresia=membresia)
+            ClienteDAO.insertar(cliente)
+            showinfo(title='Agregar', message='Cliente agregado...')
+        else: # Actualizar
+            cliente = Cliente(id_cliente=self.id_cliente, nombre=nombre, apellido=apellido, membresia=membresia)
+            ClienteDAO.actualizar(cliente)
+            showinfo(title='Actualizar', message='Cliente actualizado')
+        # volvemos a mostrar los datos y limpiamos el formulario 
+        self.recargar_datos()
+         
+        
+    def cargar_cliente(self,event):
+        elemto_seleccionado = self.tabla.selection()[0]
+        elemento = self.tabla.item(elemto_seleccionado)
+        cliente_t = elemento['values'] # tupla de valores del ciente seleccionado
+        # recuperar cada valodr del cliente 
+        self.id_cliente = cliente_t[0]
+        nombre = cliente_t[1]
+        apellido = cliente_t[2]
+        membresia = cliente_t[3]
+        
+        # antes de cargar, limpiamos formulario 
+        self.limpiar_formulario()
+        # cargar los valores en el formulario 
+        self.nombre_t.insert(0,nombre)
+        self.apellido_t.insert(0,apellido)
+        self.membresia_t.insert(0,membresia)
+        
+        
+    def recargar_datos(self):
+        # volver a cargar los datos de la tabla 
+        self.cargar_tabla()
+        # limpiar datos 
+        self.limpiar_datos()
+     
+        
+    def eliminar_cliente(self):
+        if self.id_cliente is None:
+            showerror(title='Atencion', message='Debes seleccionar un cliente a eliminar')
+        else:
+            cliente = Cliente(id_cliente=self.id_cliente)
+            ClienteDAO.eliminar(cliente)
+            showinfo(title='Eliminado', message='Cliente eliminado...')
+            self.recargar_datos()
+       
+            
+    def limpiar_datos(self):
+        self.limpiar_formulario()
+        self.id_cliente = None
+
+        
+    def limpiar_formulario(self):
+        self.nombre_t.delete(0,tk.END)
+        self.apellido_t.delete(0,tk.END)
+        self.membresia_t.delete(0,tk.END)
         
 if __name__ == "__main__":
     
